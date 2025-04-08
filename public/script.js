82cb5c1d30ca8c8e0db1289b699e4b0a9b701972
KevinDASC22 <109813046+KevinDASC22@users.noreply.github.com>
2025-04-08 18:39:07 +0000

    ehh

--- public/script.js
+++ public/script.js
@@ -1,5 +1,25 @@
 // Create a function that tests the post route handler [2 pts]
+async function newPotion(){
+    try{
+        const response = await fetch(base+"",{
+            method:"POST",
+            headers:{
+                "Content-Type":"applicaton/json"
+            },
+            body:JSON.stringify({
+                label:"speed potion",
+                effect:"makes you run fast",
+                ingredients:"ripped which craft pages and goat blood",
+                color:"purple",
+                isExplosive:true,
 
+            })
+            
+        }
+
+        )
+    }
+}
 
 
 // Call the function [1 pt]
